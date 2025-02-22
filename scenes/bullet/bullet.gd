0fd10ea0ab0f041a47bf641fdc8f38a4975c8254
Evan Zhang <evanzh15@gmail.com>
2025-02-22 12:13:27 -0500

    Added a projectile trajectory
    only took me 2 days but its finished

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -2,12 +2,38 @@ extends CharacterBody2D
 
 @export var dir = 0
 @export var SPEED = 400.0
-#const JUMP_VELOCITY = 1.0
+@onready var line = $Line2D
+@onready var line2 = $Line2D2
+@onready var point = $Node2D
+@onready var ray = $RayCast2D
+
+const MAX_LEN = 2000
 
 func _ready() -> void:
-	var direction = Vector2.UP
-	velocity = direction.rotated(dir * PI / 180) * SPEED
-	rotation = dir * PI / 180
+	velocity = (point.position.rotated(global_rotation)).normalized() * SPEED
+	rotation = global_rotation
+
+func _process(delta: float) -> void:
+	line.clear_points()
+	line.add_point(Vector2.ZERO)
+	ray.global_position = global_position
+	ray.target_position = (point.position).normalized() * MAX_LEN
+	ray.force_raycast_update()
+	if ray.is_colliding():
+		var col_point = ray.get_collision_point()
+		var line_len = calc_length(col_point - global_position)
+		var normal = ray.get_collision_normal()
+		line.add_point(to_local(col_point))
+		ray.global_position = col_point
+		var target_ray = (col_point - global_position).bounce(normal).normalized() * (103 - line_len)
+		ray.target_position = target_ray + col_point
+		line.add_point(to_local(ray.target_position))
+		ray.force_raycast_update()
+	else:
+		line.add_point(ray.target_position)
+		print(global_position)
+		print(to_global(ray.target_position))
+		print(calc_length(to_global(ray.target_position) - global_position))
 
 func _physics_process(delta: float) -> void:
 	velocity += get_gravity() # for the gravity well, default is 0 so it won't affect the bullet if there is no gravity well
@@ -21,3 +47,6 @@ func _physics_process(delta: float) -> void:
 	elif collision:
 		queue_free()
 	rotation = velocity.angle() + PI / 2
+
+func calc_length(temp: Vector2) -> float:
+	return sqrt(pow(temp.x, 2) + pow(temp.y, 2))
