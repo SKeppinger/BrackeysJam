e1cfa64bd40b619619816511e65337b01b116c60
Tyler Pearson <pearsonty9@gmail.com>
2025-02-23 01:53:08 -0500

    Stuff

--- scenes/draggable/draggable_area/draggable_area.gd
+++ scenes/draggable/draggable_area/draggable_area.gd
@@ -1,22 +1,26 @@
 extends Control
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	return data is PackedScene or data is Node2D
+	if data is not Dictionary: 
+		return false
+	return data.scene is PackedScene or data.scene is Node2D
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	if data is PackedScene:
+	if data.scene is PackedScene:
 		# Create a new version of the dragged object and place it in the scene
-		var item = data.instantiate()
+		var item = data.scene.instantiate()
 		if item.name == "Portal":
-			var item_partner = data.instantiate()
+			var item_partner = data.scene.instantiate()
 			#This is some self referential bs
 			item.partner = item_partner
 			item_partner.partner = item
 			item_partner.position = at_position + Vector2(0,30)
-			item_partner.angle = 0
+			item_partner.angle = data.rotation
+			item.angle = data.rotation
 			add_child(item_partner)
 		item.position = at_position
+		item.rotation = data.rotation
 		add_child(item)
-	elif data is Node2D:
+	elif data.scene is Node2D:
 		# If the scene is a node 2d then just move it to the cursors position
-		data.position = at_position
+		data.scene.position = at_position
